5e3e899c4c3ec743e4c95d29a7b7a42cae755546
Dima <dimafax222@gmail.com>
2023-12-27 15:19:38 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header () {
+  return <h2>Hello world</h2>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
